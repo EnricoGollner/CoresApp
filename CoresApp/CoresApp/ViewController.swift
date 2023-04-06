47d62600070cccd9e7ffa98b231448de33dd5215
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-04-06 04:14:06 -0300

    Finalizado com autolayout

--- CoresApp/CoresApp/ViewController.swift
+++ CoresApp/CoresApp/ViewController.swift
@@ -9,15 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var sliderColor: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         colorView.backgroundColor = .green
-        
     }
 
-
+    @IBAction func tappedChangeToYellow(_ sender: Any) {
+        colorView.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func tappedChangeToBlue(_ sender: Any) {
+        colorView.backgroundColor = .blue
+    }
+    
+    @IBAction func tappedChangeToRed(_ sender: Any) {
+        colorView.backgroundColor = .red
+    }
+    
+    @IBAction func tappedColorsResset(_ sender: Any) {
+        colorView.backgroundColor = .green
+    }
+    
+    @IBAction func sliderAction(_ sender: Any) {
+        colorView.alpha = CGFloat(sliderColor.value)
+    }
+    
+    func showAllert() -> UIAlertController {
+        let alert = UIAlertController(title: "Teste", message: "Teste aqui", preferredStyle: .alert)
+        
+        return alert
+    }
 }
 
